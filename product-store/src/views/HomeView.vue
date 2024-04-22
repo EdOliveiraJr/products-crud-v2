02410164fe7475b7b851abab01b4d95865261212
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-04-22 13:38:40 -0300

    fix(HomeView.vue): including ProductForm on click button add product

--- product-store/src/views/HomeView.vue
+++ product-store/src/views/HomeView.vue
@@ -16,7 +16,11 @@
                 severity="success"
                 raised 
                 rounded
+                @click="addProduct"
                 />
+            </div>
+            <div v-if="this.visible == true">
+                <ProductForm :visible="this.visible" @closedDialog="visible=$event"/>
             </div>    
         </div>
         <div v-else>
@@ -31,18 +35,24 @@ import Card from 'primevue/card';
 import Button from 'primevue/button';
 import InactiveProducts from '../components/InactiveProducts.vue'
 import ActiveProducts from '../components/ActiveProducts.vue'
-
+import ProductForm from '../components/ProductForm.vue';
 
 export default {
-  components: { TabMenu, Button, Card, InactiveProducts, ActiveProducts },
+  components: { TabMenu, Button, Card, InactiveProducts, ActiveProducts, ProductForm },
   data() {
-      return {
-          label: '',
-          items: [
-              { label: 'Produtos Ativos', icon: 'pi pi-check-circle'},
-              { label: 'Produtos Inativos', icon: 'pi pi-ban'},
-          ]
-      }   
+    return {
+        label: '',
+        visible: false, 
+        items: [
+            { label: 'Produtos Ativos', icon: 'pi pi-check-circle'},
+            { label: 'Produtos Inativos', icon: 'pi pi-ban'},
+        ]
+    }   
+  },
+  methods: {
+    addProduct(){
+        this.visible = true;
+    }
   },
 }
 </script>
