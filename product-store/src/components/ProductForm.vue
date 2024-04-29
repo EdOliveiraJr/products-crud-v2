14b33d4d5b64783659ebc7f659062df1db62ec9a
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-04-29 11:21:00 -0300

    feat: implements style for ProductForm using Sass

--- product-store/src/components/ProductForm.vue
+++ product-store/src/components/ProductForm.vue
@@ -5,62 +5,58 @@
 		@after-hide="closeDialog"
 	>
 		<div v-if="!productEdit">
-			<FloatLabel class="m-5">
+			<FloatLabel class="lbl">
 				<InputText id="name" v-model="product.name" />
 				<label for="name">Nome</label>
 			</FloatLabel>
-			<FloatLabel class="m-5">
+			<FloatLabel class="lbl">
 				<InputText id="price" v-model="product.price" />
 				<label for="price">Preço</label>
 			</FloatLabel>
-			<FloatLabel class="m-5">
+			<FloatLabel class="lbl">
 				<InputText id="description" v-model="product.description" />
 				<label for="description">Descrição</label>
 			</FloatLabel>
 			<div class="flex justify-content-end gap-2">
 				<Button 
+					class="btn-form"
 					label="Cancelar"
-					raised
-					rounded
 					severity="secondary" 
 					@click="closeDialog"
 				>
 				</Button>
 				<Button 
+					class="btn-form"
 					label="Salvar"
-					raised
-					rounded 
 					@click="saveNewProduct(product)"
 				>
 				</Button>
 			</div>
 		</div>
 		<div v-else>
-			<FloatLabel class="m-5">
+			<FloatLabel class="lbl">
 				<InputText id="name" v-model="productEdit.name" />
 				<label for="name">Nome</label>
 			</FloatLabel>
-			<FloatLabel class="m-5">
+			<FloatLabel class="lbl">
 				<InputText id="price" v-model="productEdit.price" />
 				<label for="price">Preço</label>
 			</FloatLabel>
-			<FloatLabel class="m-5">
+			<FloatLabel class="lbl">
 				<InputText id="description" v-model="productEdit.description" />
 				<label for="description">Descrição</label>
 			</FloatLabel>
 			<div class="flex justify-content-end gap-2">
-				<Button 
+				<Button
+					class="btn-form"
 					label="Cancelar" 
-					raised
-					rounded
 					severity="secondary" 
 					@click="closeDialog"
 				>
 				</Button>
-				<Button 
+				<Button
+					class="btn-form"
 					label="Salvar"
-					raised
-					rounded 
 					@click="saveEditProduct(productEdit)"
 				>
 				</Button>
