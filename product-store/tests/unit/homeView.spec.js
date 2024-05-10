ba4fbcc9a261a9bf315d3057049a8cbf6aac011e
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-10 11:23:26 -0300

    feat: implements test unit for render tab Produtos Ativos in the HomeView.vue

---
+++ product-store/tests/unit/homeView.spec.js
@@ -0,0 +1,41 @@
+import HomeView from "@/views/HomeView.vue"
+import axios from "axios"
+import { mount } from "@vue/test-utils"
+
+jest.mock('axios', () => {
+  const mAxiosInstance = { get: jest.fn() };
+  
+  return {
+    create: jest.fn(() => mAxiosInstance),
+  };
+});
+
+function factory() {
+  return mount(HomeView, {
+    data() {
+      return {
+        header: 'Adicionar Novo Produto',
+        items: [
+          { label: 'Produtos Ativos', icon: 'pi pi-check-circle' },
+          { label: 'Produtos Inativos', icon: 'pi pi-ban' },
+        ], 
+        label: '',
+        productsActives: [{}],
+        productsInactives: [{}],
+        visible: false
+      }
+    }
+  })
+}
+
+describe('HomeView', ()=>{
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  it('render Tab Produtos Ativos',()=>{
+    const wrapper = factory()
+    expect(wrapper.html()).toContain('Produtos Ativos')
+  })
+
+})
