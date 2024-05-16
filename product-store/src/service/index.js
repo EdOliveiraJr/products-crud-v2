2d9133dfd594fec65153dc42cbb0c59a532225c8
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-16 10:31:26 -0300

    refactor: changes implementation of the services removing try catch block

--- product-store/src/service/index.js
+++ product-store/src/service/index.js
@@ -3,86 +3,27 @@ import api from '../plugins/axios'
 const service = {
     async getActivesProducts() {
         return await api.get('/products?isActive=true');
-        // try {
-        //     const response = await api.get('/products?isActive=true');
-        //     console.log(response);
-        //     return response.data.data;
-        // }
-        // catch{
-        //     console.log("Erro ao obter dados:", error);
-        //     return null;
-        // }
     },
     async getInactivesProducts(){
         return await api.get('/products?isActive=false');
-        // try{
-        //     const response = await api.get('/products?isActive=false');
-        //     return response.data.data;
-        // }
-        // catch{
-        //     console.error("Erro ao obter dados:", error);
-        //     return null;
-        // }
     },
     async getProduct(id){
-        try{
-            const response = await api.get(`/products/${id}`);
-            return response.data.data;
-        }
-        catch{
-            console.error("Erro ao obter dados:", error);
-            return null;
-        }
+        return await api.get(`/products/${id}`);
     },
     async addProduct(product){
-        try{
-            await api.post('/products/', product);
-            return alert('Produto adicionado com sucesso');
-        }
-        catch{
-            console.error("Erro ao realizar a operação:", error);
-            return null;
-        }
+        return await api.post('/products/', product);
     },
     async updateProduct(id, product){
-        try{
-            await api.put(`/products/${id}`, product);
-            return alert('Produto atualizado com sucesso');
-        }
-        catch{
-            console.error("Erro ao realizar operação:", error);
-            return null;
-        }
+        return await api.put(`/products/${id}`, product);
     },
     async deleteProduct(id){
-        try{
-            await api.delete(`/products/${id}`);
-            return alert('Produto excluído com sucesso');
-        }
-        catch{
-            console.error("Erro ao realizar operação:", error);
-            return null;
-        }
+        return await api.delete(`/products/${id}`);
     },
     async activeProduct(id){
-        try{
-            await api.patch(`/products/${id}/active/`);
-            return alert('Produto ativado com sucesso');
-        }
-        catch{
-            console.error("Erro ao realizar operação:", error);
-            return null;
-        }
+        return await api.patch(`/products/${id}/active/`);
     },
     async inactiveProduct(id){
-        try{
-            await api.patch(`/products/${id}/inactive/`);
-            return alert('Produto inativado com sucesso');
-        }
-        catch{
-            console.error("Erro ao operação:", error);
-            return null;
-        }        
+        return await api.patch(`/products/${id}/inactive/`);  
     },
 }
 
