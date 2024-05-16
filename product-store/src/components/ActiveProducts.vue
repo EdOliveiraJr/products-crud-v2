dd1bf9ff9439f63bf56964e53bb263b2abf5fa71
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-16 10:28:12 -0300

    feat: implements accessibility for tab movement sequence on ActiveProducts component

--- product-store/src/components/ActiveProducts.vue
+++ product-store/src/components/ActiveProducts.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="active-products" >
+		<Toast/>
 		<h2> Produtos Ativos </h2>
 		<DataView :value="products">
 			<template #list="slotProps">
@@ -11,16 +12,16 @@
 						<div class="flex flex-column sm:flex-row sm:align-items-center p-4 gap-3" 
 							:class="{'border-top-1 surface-border': index !== 0}"
 						>
-							<div class="flex flex-column md:flex-row justify-content-around md:align-items-center flex-1 gap-4">
-								<Tag :value="item.isActive ? 'Ativo' : 'Inativo'"></Tag>
+							<div  class="flex flex-column md:flex-row justify-content-around md:align-items-center flex-1 gap-4">
+								<Tag :tabindex="0" :value="item.isActive ? 'Ativo' : 'Inativo'"></Tag>
 								<div class="flex flex-row md:flex-column justify-content-between align-items-start gap-2">
-									<div>
-										<div class="text-lg font-medium text-900 mt-2">{{ item.name }}</div>
+									<div :tabindex="0">
+										<div class="text-lg font-medium text-900 mt-2">{{ item.name }} </div>
 										<span class="font-medium text-secondary text-sm">{{ item.description }}</span>
 									</div>
 								</div>
 								<div class="flex flex-column md:align-items-end gap-5">
-									<span class="text-xl font-semibold text-900">{{ formatTextToCurrency(item.price) }}</span>
+									<span class="text-xl font-semibold text-900" :tabindex="0">{{ formatTextToCurrency(item.price) }}</span>
 								</div>
 								<div>
 									<Button
