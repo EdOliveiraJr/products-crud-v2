7e7207c1b99c5e5c97e36b31a9c6f25d45a635de
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-17 12:41:12 -0300

    fix: removing unnecessary imports

--- product-store/tests/unit/plugins.js
+++ product-store/tests/unit/plugins.js
@@ -1,15 +1,7 @@
 import PrimeVue from 'primevue/config';
-import router from '@/router/index';
-import service from '@/service/index';
-import ToastService from 'primevue/toastservice';
-import api from '@/plugins/axios';
 
 const plugins = [
-  api,
   PrimeVue,
-  router,
-  service,
-  ToastService,
 ];
 
 export default plugins;
