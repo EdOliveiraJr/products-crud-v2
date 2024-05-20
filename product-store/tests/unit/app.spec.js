12f9f0d22704d3d643e1a716e66a5bcc6baafee5
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-20 12:40:07 -0300

    refactor: change tests units methods from it for test

--- product-store/tests/unit/app.spec.js
+++ product-store/tests/unit/app.spec.js
@@ -2,17 +2,17 @@ import App from "@/App.vue";
 import { mount } from "@vue/test-utils";
 
 describe('App', ()=>{
-  it('render router link Home', ()=>{
+  test('render router link Home', ()=>{
     const wrapper = mount(App)
     expect(wrapper.html()).toContain('<router-link to="/">Home</router-link>')
   })
 
-  it('render router link About', ()=>{
+  test('render router link About', ()=>{
     const wrapper = mount(App)
     expect(wrapper.html()).toContain('<router-link to="/about">About</router-link>')
   })
 
-  it('render router-view', ()=>{
+  test('render router-view', ()=>{
     const wrapper = mount(App)
     expect(wrapper.html()).toContain('router-view')
   })
