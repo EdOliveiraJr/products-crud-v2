47e4af27a2e98d0dd6f3dde9f70dd6798ee48cfe
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-21 09:05:08 -0300

    feat: implements test suite for ProductForm component

---
+++ product-store/tests/unit/ProductForm.spec.js
@@ -0,0 +1,62 @@
+import { config, mount } from "@vue/test-utils";
+
+import ProductForm from "../../src/components/ProductForm.vue";
+import setup from './setup';
+import plugins from "./plugins";
+
+config.global = setup.global;
+
+describe('ProductForm', () => {
+  test('closeDialog method change value of the attribute visibleDialog for true', ()=> {
+    const wrapper = mount(ProductForm, {
+      global: {
+        plugins
+      }
+    })
+    wrapper.vm.visibleDialog = true;
+    wrapper.vm.closeDialog();
+    expect(wrapper.vm.visibleDialog).toBeFalsy();
+  })
+
+  test('closeDialog method emits event correctly', ()=> {
+    const wrapper = mount(ProductForm, {
+      global: {
+        plugins
+      }
+    })
+    wrapper.vm.closeDialog();
+    expect(wrapper.emitted('closedDialog')).toBeTruthy();
+  })
+
+  test('closeDialog method emits value false', ()=> {
+    const wrapper = mount(ProductForm, {
+      global: {
+        plugins
+      }
+    })
+    wrapper.vm.closeDialog();
+    expect(wrapper.emitted('closedDialog')[1]).toBeFalsy();
+  })
+
+  test('saveEditProduct method emits event correctly', () => {
+    const wrapper = mount(ProductForm, {
+      global: {
+        plugins
+      }
+    })
+    const product = {name: 'Produto 1', price: '10', describe: 'Produto 1'};
+    wrapper.vm.saveEditProduct(product);
+    expect(wrapper.emitted('saveEditProduct')).toBeTruthy();
+  })
+
+  test('saveNewProduct method emits event correctly', () => {
+    const wrapper = mount(ProductForm, {
+      global: {
+        plugins
+      }
+    })
+    const product = {name: 'Produto 1', price: '10', describe: 'Produto 1'};
+    wrapper.vm.saveNewProduct(product);
+    expect(wrapper.emitted('saveNewProduct')).toBeTruthy();
+  })
+})
