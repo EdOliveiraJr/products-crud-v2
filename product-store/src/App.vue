3be74b259753c69da86f27314f45a292309846aa
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-28 08:53:53 -0300

    feat: implements new style for app nav bar and change css of the component for scss

--- product-store/src/App.vue
+++ product-store/src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <Toast/>
   <nav>
-    <Toast/>
     <h1>Gerenciador de Produtos</h1>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="page-links">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </nav>
   <router-view />
 </template>
@@ -15,26 +17,3 @@ export default {
   components: { Toast }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
