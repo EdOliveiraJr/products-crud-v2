892e11c9a375ce8e38050438a48b4e96565f52b8
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-28 09:51:39 -0300

    feat: implements Footer component

--- product-store/src/App.vue
+++ product-store/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <Toast/>
-  <nav>
-    <h1>Gerenciador de Produtos</h1>
-    <div class="page-links">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-  </nav>
-  <router-view />
-</template>
+  <main>
+    <nav>
+      <h1>Gerenciador de Produtos</h1>
+      <div class="page-links">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </nav>
+    <router-view />
+  </main>
+  <Footer/>
+  </template>
 
 <script>
+import Footer from './components/Footer.vue'
 import Toast from 'primevue/toast';
 
 export default {
-  components: { Toast }
+  components: { Footer, Toast }
 }
 </script>
---
+++ product-store/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footer">
+    <p>Developed by Ed Oliveira 2024®</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
